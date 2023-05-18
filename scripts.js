482835302058faa8048b8db9c7c2a73b22e0ecd9
Tiffany M <67019104+tiffany-m@users.noreply.github.co>
2023-05-18 12:32:58 -0400

    Add validation for empty text fields

--- scripts.js
+++ scripts.js
@@ -2,11 +2,15 @@ let userPassword = document.getElementById("user_password");
 let confirmPassword = document.getElementById("confirm_password");
 let submitButton = document.getElementById("submit_btn")
 let passwordInput = document.querySelector(".password-input");
+let inputTexts = document.querySelectorAll(".input-text")
 
 let errorMessage = document.createElement("div");
 errorMessage.textContent = "Passwords do not match.";
 errorMessage.style.cssText = "align-self: center; color: red; font-weight: bold;";
 
+let requiredMessage = document.createElement("span");
+requiredMessage.textContent = "*Required";
+requiredMessage.style.cssText = "color: red; font-weight: bold;";
 
 submitButton.addEventListener("click", () => {
     if(userPassword != confirmPassword) {
@@ -14,7 +18,16 @@ submitButton.addEventListener("click", () => {
         confirmPassword.classList.add("error");
         passwordInput.appendChild(errorMessage);
     }
+
+    inputTexts.forEach(input => {
+        if(input.textContent == "") {
+            input.style.color = "red";
+            input.value = "*Required";
+        }
+    })
 })
+
+console.log()
 
 userPassword.addEventListener("click", () => {
     if(errorMessage) {
