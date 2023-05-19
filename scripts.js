55bf534548e262c31352fd8188d71db1b345681c
Tiffany M <67019104+tiffany-m@users.noreply.github.co>
2023-05-19 16:20:32 -0400

    Various password validation

--- scripts.js
+++ scripts.js
@@ -17,7 +17,9 @@ let confirmPw = document.querySelector(".confirmPw");
 let submitButton = document.getElementById("submit_btn")
 let passwordInput = document.querySelector(".password-input");
 
-submitButton.addEventListener("click", () => {
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
     if (firstName.value === "") {
         fName.classList.remove("hidden");
     } else {
@@ -54,35 +56,91 @@ submitButton.addEventListener("click", () => {
         confirmPw.classList.add("hidden");
     }
 })
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
 
-// let errorMessage = document.createElement("div");
-// errorMessage.textContent = "Passwords do not match.";
-// errorMessage.style.cssText = "align-self: center; color: red; font-weight: bold;";
+// When the user clicks on the password field, show the message box
+userPassword.onfocus = function () {
+    document.getElementById("message").style.display = "block";
+}
 
-// submitButton.addEventListener("click", () => {
-//     if(userPassword != confirmPassword) {
-//         userPassword.classList.add("error");
-//         confirmPassword.classList.add("error");
-//         passwordInput.appendChild(errorMessage);
-//     }
-// })
+// When the user clicks outside of the password field, hide the message box
+userPassword.onblur = function () {
+    document.getElementById("message").style.display = "none";
+}
 
-// userPassword.addEventListener("click", () => {
-//     if(errorMessage) {
-//         passwordInput.removeChild(errorMessage);
-//         userPassword.classList.toggle("error");
-//         confirmPassword.classList.toggle("error");
-//         userPassword.textContent = "";
-//         confirmPassword.textContent ="";
-//     }
-// })
+// When the user starts to type something inside the password field
+userPassword.onkeyup = function () {
+    // Validate lowercase letters
+    var lowerCaseLetters = /[a-z]/g;
+    if (userPassword.value.match(lowerCaseLetters)) {
+        letter.classList.remove("invalid");
+        letter.classList.add("valid");
+    } else {
+        letter.classList.remove("valid");
+        letter.classList.add("invalid");
+    }
 
-// confirmPassword.addEventListener("click", () => {
-//     if (errorMessage) {
-//         passwordInput.removeChild(errorMessage);
-//         userPassword.classList.toggle("error");
-//         confirmPassword.classList.toggle("error");
-//         userPassword.textContent = "";
-//         confirmPassword.textContent = "";
-//     }
-// })
+    // Validate capital letters
+    var upperCaseLetters = /[A-Z]/g;
+    if (userPassword.value.match(upperCaseLetters)) {
+        capital.classList.remove("invalid");
+        capital.classList.add("valid");
+    } else {
+        capital.classList.remove("valid");
+        capital.classList.add("invalid");
+    }
+
+    // Validate numbers
+    var numbers = /[0-9]/g;
+    if (userPassword.value.match(numbers)) {
+        number.classList.remove("invalid");
+        number.classList.add("valid");
+    } else {
+        number.classList.remove("valid");
+        number.classList.add("invalid");
+    }
+
+    // Validate length
+    if (userPassword.value.length >= 8) {
+        length.classList.remove("invalid");
+        length.classList.add("valid");
+    } else {
+        length.classList.remove("valid");
+        length.classList.add("invalid");
+    }
+}
+
+let errorMessage = document.createElement("div");
+errorMessage.textContent = "Passwords do not match.";
+errorMessage.style.cssText = "align-self: center; color: red; font-weight: bold;";
+
+submitButton.addEventListener("click", () => {
+    if((userPassword != confirmPassword) && (userPassword.value != "") && (confirmPassword.value != "") ) {
+        userPassword.classList.add("error");
+        confirmPassword.classList.add("error");
+        passwordInput.appendChild(errorMessage);
+    }
+})
+
+userPassword.addEventListener("click", () => {
+    if(errorMessage) {
+        passwordInput.removeChild(errorMessage);
+        userPassword.classList.toggle("error");
+        confirmPassword.classList.toggle("error");
+        userPassword.textContent = "";
+        confirmPassword.textContent ="";
+    }
+})
+
+confirmPassword.addEventListener("click", () => {
+    if (errorMessage) {
+        passwordInput.removeChild(errorMessage);
+        userPassword.classList.toggle("error");
+        confirmPassword.classList.toggle("error");
+        userPassword.textContent = "";
+        confirmPassword.textContent = "";
+    }
+})
